f1f077f876647e53644f4f1804f63a9ecc8e5d0d
Nikolett Uracs <loveninagold@gmail.com>
2017-01-13 14:14:51 +0100

    plastic garbage edited

--- plastic_garbage.py
+++ plastic_garbage.py
@@ -2,4 +2,12 @@ from garbage import Garbage
 
 
 class PlasticGarbage(Garbage):
-    pass
+
+    def __init__(self, name, is_clean):
+        self.name = name
+        self.is_clean = is_clean
+
+    def clean(self):
+        if self.is_clean is False:
+            self.is_clean = True
+            return self.is_clean
